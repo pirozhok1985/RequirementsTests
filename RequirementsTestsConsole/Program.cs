63eb21ca3cd75528ecd868460e91821a69bf7acb
evgenii anisimov <pirozhok1985@gmail.com>
2022-03-02 22:42:56 +0300

    Создал заготовки для проекта

---
+++ RequirementsTestsConsole/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Threading.Channels;
+using RequirementsTestsServices.UseCases.LinuxInfo;
+
+var hwinfo = new HardwareInfo();
+var vendorinfo = hwinfo.GetVendorInfo();
+Console.WriteLine("Here is vendor info {0}", vendorinfo.Value);
