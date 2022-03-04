9612cec500c233a5d6dd1a9bb8c06b9fb4e55159
evgenii anisimov <pirozhok1985@gmail.com>
2022-03-04 19:04:39 +0300

    Реализация методов GetOsInfo() и GetNetworkConfigInfo()

--- RequirementsTestsServices/UseCases/Interfaces/IGetOsInfo.cs
+++ RequirementsTestsServices/UseCases/Interfaces/IGetOsInfo.cs
@@ -4,9 +4,9 @@ namespace RequirementsTestsServices.UseCases.Interfaces;
 
 public interface IGetOsInfo
 {
-    public Info<Dictionary<string,string>> GetOsInfo();
-    public Info<Dictionary<string,Dictionary<string,string[]>>> GetNetworkConfigInfo();
-    public Info<Dictionary<string,string>> GetDiskDrivePartitionInfo();
+    public Task<Info<Dictionary<string,string>>> GetOsInfo();
+    public Task<Info<Dictionary<string,Dictionary<string,string[]>>>> GetNetworkConfigInfo();
+    public Task<Info<Dictionary<string,string>>> GetDiskDrivePartitionInfo();
     // public Info<T> GetDiskDriveEncryptionInfo();
     // public Info<T> GetAdDomainInfo();
     // public Info<T> GetRootCertInfo();
--- RequirementsTestsServices/UseCases/LinuxInfo/OperatingSystemInfo.cs
+++ RequirementsTestsServices/UseCases/LinuxInfo/OperatingSystemInfo.cs
@@ -7,30 +7,34 @@ namespace RequirementsTestsServices.UseCases.LinuxInfo;
 public class OperatingSystemInfo : IGetOsInfo
 {
     private const string CategoryName = "Operating system";
-    public Info<Dictionary<string, string>> GetOsInfo()
+    public async Task<Info<Dictionary<string, string>>> GetOsInfo()
     {
-        throw new NotImplementedException();
+        var result = await LinuxInfoHelpers.ReadToTheEndAsync("/etc/lsb-release");
+        var query = result.Split("\n").Where(e => e != String.Empty).ToDictionary(k => k.Split("=")[0],v => v.Split("=")[1]);
+        return LinuxInfoHelpers.GenerateInfo(query, CategoryName, "Os Info");
     }
 
-    public Info<Dictionary<string, Dictionary<string,string[]>>> GetNetworkConfigInfo()
+    public async Task<Info<Dictionary<string, Dictionary<string,string[]>>>> GetNetworkConfigInfo()
     {
         var result = new Dictionary<string, Dictionary<string,string[]>>();
-        var nics = NetworkInterface.GetAllNetworkInterfaces()
-            .Where(i => !i.Name.Contains("lo"))
-            .Where(i => !i.Name.Contains("docker"));
-        foreach (var nic in nics)
+        await Task.Run(() =>
         {
-            var dict = new Dictionary<string, string[]>();
-            dict.Add("Ip Address", nic.GetIPProperties().UnicastAddresses.Select(a => a.Address.ToString()).ToArray());
-            dict.Add("Gateway", nic.GetIPProperties().GatewayAddresses.Select(g => g.Address.ToString()).ToArray());
-            dict.Add("Dns Servers", nic.GetIPProperties().DnsAddresses.Select(d => d.ToString()).ToArray());
-            result.Add(nic.Name,dict);
-        }
-
+            var nics = NetworkInterface.GetAllNetworkInterfaces()
+                .Where(i => !i.Name.Contains("lo"))
+                .Where(i => !i.Name.Contains("docker"));
+            foreach (var nic in nics)
+            {
+                var dict = new Dictionary<string, string[]>();
+                dict.Add("Ip Address", nic.GetIPProperties().UnicastAddresses.Select(a => a.Address.ToString()).ToArray());
+                dict.Add("Gateway", nic.GetIPProperties().GatewayAddresses.Select(g => g.Address.ToString()).ToArray());
+                dict.Add("Dns Servers", nic.GetIPProperties().DnsAddresses.Select(d => d.ToString()).ToArray());
+                result.Add(nic.Name,dict);
+            }
+        });
         return LinuxInfoHelpers.GenerateInfo(result,CategoryName,"Network Config");
     }
 
-    public Info<Dictionary<string, string>> GetDiskDrivePartitionInfo()
+    public Task<Info<Dictionary<string, string>>> GetDiskDrivePartitionInfo()
     {
         throw new NotImplementedException();
     }
