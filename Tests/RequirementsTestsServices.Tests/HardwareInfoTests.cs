6826acf74bac3d6d25eb9395a438aa9ca3f7fb14
evgenii anisimov <pirozhok1985@gmail.com>
2022-03-04 08:23:08 +0300

    Реализовал тесты для всех методов класса HarwareInfo

--- Tests/RequirementsTestsServices.Tests/HardwareInfoTests.cs
+++ Tests/RequirementsTestsServices.Tests/HardwareInfoTests.cs
@@ -10,20 +10,76 @@ namespace RequirementsTestsServices.Tests;
 [TestClass]
 public class HardwareInfoTests
 {
-    [TestMethod]
-    public async Task CheckIfReturnsCorrectValue()
-    {
-        var expectedCategory = "Hardware";
+    private const string ExpectedCategory = "Hardware";
 
+    [TestMethod]
+    public async Task CheckIfGetModelInfoReturnsCorrectValue()
+    {
         var resultModel = await new HardwareInfo().GetModelInfoAsync();
-        var resultRam = await new HardwareInfo().GetRamInfoAsync(new[] {"MemTotal","MemFree","Mapped"});
 
         Assert.IsType<Info<string>>(resultModel);
         Assert.NotEmpty(resultModel.Value!);
-        Assert.Equal(expectedCategory,resultModel.Category!.Name);
+        Assert.Equal(ExpectedCategory,resultModel.Category!.Name);
+    }  
+    
+    [TestMethod]
+    public async Task CheckIfGetVendorInfoReturnsCorrectValue()
+    {
+        var resultVendor = await new HardwareInfo().GetVendorInfoAsync();
+
+        Assert.IsType<Info<string>>(resultVendor);
+        Assert.NotEmpty(resultVendor.Value!);
+        Assert.Equal(ExpectedCategory,resultVendor.Category!.Name);
+    }    
+    
+    [TestMethod]
+    public async Task CheckIfGetSerialNumberInfoReturnsCorrectValue() //Permission denied - Test Failed!
+    {
+        var resultSerial = await new HardwareInfo().GetSerialNumberInfoAsync();
+
+        Assert.IsType<Info<string>>(resultSerial);
+        Assert.NotEmpty(resultSerial.Value!);
+        Assert.Equal(ExpectedCategory,resultSerial.Category!.Name);
+    }  
+    
+    [TestMethod]
+    public async Task CheckIfGetRamInfoReturnsCorrectValue()
+    {
+        var resultRam = await new HardwareInfo().GetRamInfoAsync(new[] {"MemTotal","MemFree","Mapped"});
 
         Assert.IsType<Info<Dictionary<string,long>>>(resultRam);
         Assert.NotEmpty(resultRam.Value!);
-        Assert.Equal(expectedCategory,resultRam.Category!.Name);
+        Assert.Equal(ExpectedCategory,resultRam.Category!.Name);
+    }  
+    
+    [TestMethod]
+    public async Task CheckIfGetCpuInfoReturnsCorrectValue()
+    {
+        var resultCpu = await new HardwareInfo().GetCpuInfoAsync(new[] {"model name", "cpu cores"});
+
+        Assert.IsType<Info<Dictionary<string,string>>>(resultCpu);
+        Assert.NotEmpty(resultCpu.Value!);
+        Assert.Equal(ExpectedCategory,resultCpu.Category!.Name);
+    }    
+    
+    [TestMethod]
+    public async Task CheckIfGetDiskDriveInfoReturnsCorrectValue()
+    {
+        var resultDiskDrive = await new HardwareInfo().GetDiskDriveInfoAsync();
+
+        Assert.IsType<Info<Dictionary<string,string>>>(resultDiskDrive);
+        Assert.NotEmpty(resultDiskDrive.Value!);
+        Assert.Equal(ExpectedCategory,resultDiskDrive.Category!.Name);
+    }   
+    
+    [TestMethod]
+    public async Task CheckIfGetFirmwareInfoReturnsCorrectValue()
+    {
+        var resultFirmWare = await new HardwareInfo().GetFirmWareInfoAsync();
+
+        Assert.IsType<Info<Dictionary<string,string>>>(resultFirmWare);
+        Assert.NotEmpty(resultFirmWare.Value!);
+        Assert.Equal(ExpectedCategory,resultFirmWare.Category!.Name);
     }
+    
 }
