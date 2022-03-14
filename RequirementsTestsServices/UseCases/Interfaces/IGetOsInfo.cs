bf5f7bbec244409c51535818d0a11ff887cb2424
evgenii anisimov <pirozhok1985@gmail.com>
2022-03-14 19:46:41 +0300

    Изменил интерфейс IGetOsInfo так, чтобы его методы возвращали типы, олицетворяющие конкретный тип информации

--- RequirementsTestsServices/UseCases/Interfaces/IGetOsInfo.cs
+++ RequirementsTestsServices/UseCases/Interfaces/IGetOsInfo.cs
@@ -1,12 +1,13 @@
 using RequirementsTestsDomain.Models;
+using RequirementsTestsServices.UseCases.InventoryInfoTypes;
 
 namespace RequirementsTestsServices.UseCases.Interfaces;
 
 public interface IGetOsInfo
 {
-    public Task<Info<Dictionary<string,string>>> GetOsInfo();
-    public Task<Info<Dictionary<string,Dictionary<string,string[]>>>> GetNetworkConfigInfo();
-    public Task<Info<Dictionary<string,string>>> GetDiskDrivePartitionInfo();
+    public Task<Info<OsInfo>> GetOsInfo();
+    public Task<Info<IList<NetworkConfigInfo>>> GetNetworkConfigInfo();
+    public Task<Info<IList<DiskDrivePartitionInfo>>> GetDiskDrivePartitionInfo();
     // public Info<T> GetDiskDriveEncryptionInfo();
     // public Info<T> GetAdDomainInfo();
     // public Info<T> GetRootCertInfo();
