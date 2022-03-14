cb4e783dfe4ac5a7780d2352184ea2ce5d0d9daf
evgenii anisimov <pirozhok1985@gmail.com>
2022-03-14 19:48:15 +0300

    Частично реализовал интерфейс IGetOsInfo

--- RequirementsTestsServices/UseCases/LinuxInfo/OperatingSystemInfo.cs
+++ RequirementsTestsServices/UseCases/LinuxInfo/OperatingSystemInfo.cs
@@ -1,22 +1,45 @@
 using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
 using RequirementsTestsDomain.Models;
 using RequirementsTestsServices.UseCases.Interfaces;
+using RequirementsTestsServices.UseCases.InventoryInfoTypes;
 
 namespace RequirementsTestsServices.UseCases.LinuxInfo;
 
 public class OperatingSystemInfo : IGetOsInfo
 {
     private const string CategoryName = "Operating system";
-    public async Task<Info<Dictionary<string, string>>> GetOsInfo()
+    public async Task<Info<OsInfo>> GetOsInfo()
     {
+        var osInfo = new OsInfo();
         var result = await LinuxInfoHelpers.ReadToTheEndAsync("/etc/lsb-release");
-        var query = result.Split("\n").Where(e => e != String.Empty).ToDictionary(k => k.Split("=")[0],v => v.Split("=")[1]);
-        return LinuxInfoHelpers.GenerateInfo(query, CategoryName, "Os Info");
+        var query = result.Split("\n").Where(e => e != String.Empty)
+            .ToDictionary(k => k.Split("=")[0],v => v.Split("=")[1]);
+        foreach (var kv in query)
+        {
+            switch (kv.Key)
+            {
+                case "DISTRIB_CODENAME":
+                    osInfo.CodeName = kv.Value;
+                    break;
+                case "DISTRIB_ID":
+                    osInfo.Id = kv.Value;
+                    break;
+                case "DISTRIB_RELEASE":
+                    osInfo.Id = kv.Value;
+                    break;
+                case "DISTRIB_DESCRIPTION":
+                    osInfo.Id = kv.Value;
+                    break;
+            }
+        }
+        return LinuxInfoHelpers.GenerateInfo(osInfo, CategoryName, "Os Info");
     }
 
-    public async Task<Info<Dictionary<string, Dictionary<string,string[]>>>> GetNetworkConfigInfo()
+    public async Task<Info<IList<NetworkConfigInfo>>> GetNetworkConfigInfo()
     {
-        var result = new Dictionary<string, Dictionary<string,string[]>>();
+        IList<NetworkConfigInfo> result = new List<NetworkConfigInfo>();
+        var netConfig = new NetworkConfigInfo();
         await Task.Run(() =>
         {
             var nics = NetworkInterface.GetAllNetworkInterfaces()
@@ -24,18 +47,52 @@ public class OperatingSystemInfo : IGetOsInfo
                 .Where(i => !i.Name.Contains("docker"));
             foreach (var nic in nics)
             {
-                var dict = new Dictionary<string, string[]>();
-                dict.Add("Ip Address", nic.GetIPProperties().UnicastAddresses.Select(a => a.Address.ToString()).ToArray());
-                dict.Add("Gateway", nic.GetIPProperties().GatewayAddresses.Select(g => g.Address.ToString()).ToArray());
-                dict.Add("Dns Servers", nic.GetIPProperties().DnsAddresses.Select(d => d.ToString()).ToArray());
-                result.Add(nic.Name,dict);
+                netConfig.IpAddresses = nic.GetIPProperties().UnicastAddresses.Select(a => a.Address.ToString()).ToArray();
+                netConfig.Gateway = nic.GetIPProperties().GatewayAddresses.Select(g => g.Address.ToString()).FirstOrDefault()!;
+                netConfig.DnsServers = nic.GetIPProperties().DnsAddresses.Select(d => d.ToString()).ToArray();
+                netConfig.InterfaceName = nic.Name;
+                result.Add(netConfig);
             }
         });
         return LinuxInfoHelpers.GenerateInfo(result,CategoryName,"Network Config");
     }
-
-    public Task<Info<Dictionary<string, string>>> GetDiskDrivePartitionInfo()
+    
+    public async Task<Info<IList<DiskDrivePartitionInfo>>> GetDiskDrivePartitionInfo()
     {
-        throw new NotImplementedException();
+        IList<DiskDrivePartitionInfo> result = new List<DiskDrivePartitionInfo>();
+        await Task.Run(() =>
+        {
+            var partInfo = new DiskDrivePartitionInfo();
+            var devNameDir = Directory.EnumerateDirectories("/sys/block/").Where(d => !d.Contains("loop"));
+            foreach (var devDir in devNameDir)
+            {
+                var devName = @$"/dev/{devDir.Split("/").Last()}";
+                var partProbeRef = LinuxInfoHelpers.NewProbeFromFilename(devName);
+                var pList = LinuxInfoHelpers.GetPartitions(partProbeRef);
+                var partNum = LinuxInfoHelpers.GetNumberOfPartitions(pList);
+                for (int i = 0; i < partNum; i++)
+                {
+                    var pPart = LinuxInfoHelpers.GetPartition(pList, i);
+                    unsafe
+                    {
+                        partInfo.PartUuid = Marshal.PtrToStringAnsi((IntPtr) LinuxInfoHelpers.GetUuid(pPart))!;
+                        partInfo.PartLabel = Marshal.PtrToStringAnsi((IntPtr) LinuxInfoHelpers.GetName(pPart))!;
+                    }
+
+                    var partName = devName.Contains("nvme") ? $"{devName}p{i + 1}" : $"{devName}{i + 1}";
+                    var devProbeRef = LinuxInfoHelpers.NewProbeFromFilename(@$"/dev/{partName.Split("/").Last()}");
+                    LinuxInfoHelpers.DoFullProbe(devProbeRef);
+                    var retCode1 = LinuxInfoHelpers.LookupValue(devProbeRef, "LABEL", out string label, IntPtr.Zero);
+                    var retCode2 = LinuxInfoHelpers.LookupValue(devProbeRef, "UUID", out string uuid, IntPtr.Zero);
+                    var retCode5 = LinuxInfoHelpers.LookupValue(devProbeRef, "TYPE", out string type, IntPtr.Zero);
+                    partInfo.Label = label;
+                    partInfo.Type = type;
+                    partInfo.Name = partName;
+                    partInfo.Uuid = uuid;
+                }
+                result.Add(partInfo);
+            }
+        });
+        return LinuxInfoHelpers.GenerateInfo(result,CategoryName,"Partition configuration");;
     }
 }
