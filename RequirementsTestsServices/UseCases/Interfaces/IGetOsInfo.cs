df54690d82002102d0ef3eae0a83535e2d69ecc0
evgenii anisimov <pirozhok1985@gmail.com>
2022-03-14 21:22:08 +0300

    Добавил суффикс Async для асинхронных методов интерфейса IGetOsInfo

--- RequirementsTestsServices/UseCases/Interfaces/IGetOsInfo.cs
+++ RequirementsTestsServices/UseCases/Interfaces/IGetOsInfo.cs
@@ -5,9 +5,9 @@ namespace RequirementsTestsServices.UseCases.Interfaces;
 
 public interface IGetOsInfo
 {
-    public Task<Info<OsInfo>> GetOsInfo();
-    public Task<Info<IList<NetworkConfigInfo>>> GetNetworkConfigInfo();
-    public Task<Info<IList<DiskDrivePartitionInfo>>> GetDiskDrivePartitionInfo();
+    public Task<Info<OsInfo>> GetOsInfoAsync();
+    public Task<Info<IList<NetworkConfigInfo>>> GetNetworkConfigInfoAsync();
+    public Task<Info<IList<DiskDrivePartitionInfo>>> GetDiskDrivePartitionInfoAsync();
     // public Info<T> GetDiskDriveEncryptionInfo();
     // public Info<T> GetAdDomainInfo();
     // public Info<T> GetRootCertInfo();
