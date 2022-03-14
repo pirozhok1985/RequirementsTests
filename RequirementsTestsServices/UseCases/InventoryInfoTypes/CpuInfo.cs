3a50b4542df1de29eb9c4b2b1f251518b23b1d0a
evgenii anisimov <pirozhok1985@gmail.com>
2022-03-14 21:19:19 +0300

    Создал классы CpuInfo, RamInfo, FirmwareInfo

---
+++ RequirementsTestsServices/UseCases/InventoryInfoTypes/CpuInfo.cs
@@ -0,0 +1,7 @@
+namespace RequirementsTestsServices.UseCases.InventoryInfoTypes;
+
+public class CpuInfo
+{
+    public string? Model { get; set; }
+    public int CoresCount { get; set; }
+}
---
+++ RequirementsTestsServices/UseCases/InventoryInfoTypes/RamInfo.cs
@@ -0,0 +1,7 @@
+namespace RequirementsTestsServices.UseCases.InventoryInfoTypes;
+
+public class RamInfo
+{
+    public long Total { get; set; }
+    public long Free { get; set; }
+}
