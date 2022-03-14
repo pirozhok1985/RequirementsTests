9e9403b921c47b5d32d3e7393b50106b7536fb7e
evgenii anisimov <pirozhok1985@gmail.com>
2022-03-14 19:43:22 +0300

    Добавил класс OsInfo

---
+++ RequirementsTestsServices/UseCases/InventoryInfoTypes/OsInfo.cs
@@ -0,0 +1,9 @@
+namespace RequirementsTestsServices.UseCases.InventoryInfoTypes;
+
+public class OsInfo
+{
+    public string Id { get; set; } = String.Empty;
+    public string Release { get; set; } = String.Empty;
+    public string CodeName { get; set; } = String.Empty;
+    public string Description { get; set; } = String.Empty;
+}
