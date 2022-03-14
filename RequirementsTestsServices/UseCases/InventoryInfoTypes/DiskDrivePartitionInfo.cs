9a4c56e0e78a1a03fcb8eb602d9c33a6cbb74d5b
evgenii anisimov <pirozhok1985@gmail.com>
2022-03-14 19:47:31 +0300

    Добавил класс DiskDrivePartitionInfo

---
+++ RequirementsTestsServices/UseCases/InventoryInfoTypes/DiskDrivePartitionInfo.cs
@@ -0,0 +1,11 @@
+namespace RequirementsTestsServices.UseCases.InventoryInfoTypes;
+
+public class DiskDrivePartitionInfo
+{
+    public string Name { get; set; } = String.Empty;
+    public string PartLabel { get; set; } = String.Empty;
+    public string PartUuid { get; set; } = String.Empty;
+    public string Type { get; set; } = String.Empty;
+    public string Label { get; set; } = String.Empty;
+    public string Uuid { get; set; } = String.Empty;
+}
