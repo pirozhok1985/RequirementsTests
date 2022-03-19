07bdb3b685697fa02ab1d5b5934e3a320662fa11
evgenii anisimov <pirozhok1985@gmail.com>
2022-03-19 20:23:07 +0300

    CertificateInfo: свойство Name теперь nullable

--- RequirementsTestsDomain/Models/InventoryInfoTypes/CertificateInfo.cs
+++ RequirementsTestsDomain/Models/InventoryInfoTypes/CertificateInfo.cs
@@ -2,7 +2,7 @@ namespace RequirementsTestsDomain.Models.InventoryInfoTypes;
 
 public class CertificateInfo
 {
-    public string Name { get; set; }
+    public string? Name { get; set; }
     public DateTime EffectiveDate { get; set; }
     public DateTime ExpirationDate { get; set; }
 }
