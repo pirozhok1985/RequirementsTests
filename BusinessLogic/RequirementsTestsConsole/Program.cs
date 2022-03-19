2d86c5384ee857a8f3c1907a50b7c466621b13c6
evgenii anisimov <pirozhok1985@gmail.com>
2022-03-19 20:50:33 +0300

    Реструктуризация папок решения

---
+++ BusinessLogic/RequirementsTestsConsole/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Net.NetworkInformation;
+using System.Security.Cryptography.X509Certificates;
+using RequirementsTestsDomain.Models.InventoryInfoTypes;
+
+
+
+Console.ReadLine();
+
