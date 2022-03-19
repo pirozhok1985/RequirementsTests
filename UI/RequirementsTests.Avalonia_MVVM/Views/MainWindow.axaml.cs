08ed74d3d78a916254cb1ac96c076bf3c39fca6d
evgenii anisimov <pirozhok1985@gmail.com>
2022-03-19 21:18:53 +0300

    Добавил UI проект на Avalonia framework

---
+++ UI/RequirementsTests.Avalonia_MVVM/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace RequirementsTests.Avalonia_MVVM.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
