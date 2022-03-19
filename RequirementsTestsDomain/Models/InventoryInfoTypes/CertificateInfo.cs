0436f1bd18d53e366d0762edb0964708a986350c
evgenii anisimov <pirozhok1985@gmail.com>
2022-03-19 19:16:12 +0300

    Создал класс CertificateInfo

---
+++ RequirementsTestsDomain/Models/InventoryInfoTypes/CertificateInfo.cs
@@ -0,0 +1,8 @@
+namespace RequirementsTestsDomain.Models.InventoryInfoTypes;
+
+public class CertificateInfo
+{
+    public string Name { get; set; }
+    public DateTime EffectiveDate { get; set; }
+    public DateTime ExpirationDate { get; set; }
+}
