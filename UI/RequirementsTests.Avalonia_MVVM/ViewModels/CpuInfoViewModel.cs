459ce1ea1c7db44fb71c67ab00c2f7d1545b7c50
evgenii anisimov <pirozhok1985@gmail.com>
2022-03-23 20:03:30 +0300

    Удалил проект на Avalonia

--- UI/RequirementsTests.Avalonia_MVVM/ViewModels/CpuInfoViewModel.cs
+++
@@ -1,7 +0,0 @@
-namespace RequirementsTests.Avalonia_MVVM.ViewModels;
-
-public class CpuInfoViewModel
-{
-    public string? Model { get; set; }
-    public string? CoresCount { get; set; }
-}
--- UI/RequirementsTests.Avalonia_MVVM/ViewModels/OsInfoViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace RequirementsTests.Domain.Models.InventoryInfoTypes;
-
-public class OsInfo
-{
-    public string? Release { get; set; }
-    public string? CodeName { get; set; }
-    public string? Description { get; set; }
-}
